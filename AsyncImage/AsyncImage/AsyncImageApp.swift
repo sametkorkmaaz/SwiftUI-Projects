ff7fd34f6bf41dc82dff6b587d0c9bf7e6d64f54
sametkorkmaaz <smt.krkmz.2002@gmail.com>
2025-11-16 17:15:34 +0300

    AsyncImage project created.

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Samet Korkmaz on 16.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsyncImage/AsyncImage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsyncImage
+//
+//  Created by Samet Korkmaz on 16.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
